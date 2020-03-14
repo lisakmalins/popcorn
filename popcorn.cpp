ba819b3ea5a47aeb1a449a441761855a2bd1c0f5
Lisa Malins <lkmalins@ucdavis.edu>
2020-03-14 00:26:34 -0700

    Main file popcorn.cpp now checks each character of fasta file. Dies politely if it finds any unexpected whitespace, nonalphabetical characters, or letters that are not amino acids. Also, converts all letters to uppercase.

--- popcorn.cpp
+++ popcorn.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <algorithm>
 #include "kernels.h"
 
+/* Parse a fasta line. Check if all characters are valid and cast to uppercase.
+** Parameters: Amino acid sequence to check.
+** Returns: Sanitized amino acid sequence.
+*/
+std::string valid_AA(std::string seq) {
+  std::string sanitized;
+
+  static const char amino_acids[] =
+  { 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y' };
+
+  for (auto c : seq) {
+
+    // First check if alphabetical
+    if (!isalpha(c)) {
+      throw runtime_error("ERROR: Character " + std::string(1, c) + " not a valid amino acid");
+    }
+
+    // Convert to uppercase if necessary
+    if (islower(c)) {
+      c = toupper(c);
+    }
+
+    // Check if in amino acid alphabet
+    if (std::find(std::begin(amino_acids), std::end(amino_acids), c) < std::end(amino_acids)) {
+      sanitized += c;
+    } else {
+      throw runtime_error("ERROR: Character " + std::string(1, c) + " not a valid amino acid");
+    }
+  }
+
+  return sanitized;
+}
 
 /* Parse a valid single-fasta file.
 ** Throw runtime error if first line is not a header or if there is more than one header.
@@ -18,7 +51,6 @@ std::string parse_fasta(std::string filename) {
     throw runtime_error("ERROR: Unable to open file " + filename + "\n");
   }
 
-
   // Read first line; should be fasta header
   std::string line;
   std::getline(input_file, line);
@@ -35,6 +67,15 @@ std::string parse_fasta(std::string filename) {
       cout << "Unexpected input: " <<  line << endl;
       throw runtime_error("ERROR: Multifasta files not supported.\n"
       "Please choose one fasta sequence from " + filename + " and save as single fasta.\n");
+    }
+
+    // Check sequence and sanitize
+    try {
+      line = valid_AA(line);
+    }
+    catch (const runtime_error& e) {
+        cout << endl << e.what();
+        throw runtime_error("ERROR: Problem parsing " + filename + "\n");
     }
 
     // Concatenate line to sequence
@@ -60,8 +101,7 @@ int main(int argc, char *argv[])
     seq1 = parse_fasta(argv[1]);
     seq2 = parse_fasta(argv[2]);
   }
-  catch (const runtime_error& e)
-  {
+  catch (const runtime_error& e) {
       cout << endl << e.what();
       cout << usage << endl;
       return 1;
